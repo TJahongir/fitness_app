9064f805aec1a18e25f28a45f838e67c546f1fe2
TJahongir <jakhongirt@gmail.com>
2023-01-07 10:47:09 +0500

    hero section setup

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import { useState } from "react";
+import { Box } from "@mui/system";
+import Exercises from "../components/Exercises";
+import SearchExercises from "../components/SearchExercises";
+import HeroBanner from "../components/HeroBanner";
 
 const Home = () => {
-	return <div>Home</div>;
+	return (
+		<Box>
+			<HeroBanner />
+			<SearchExercises />
+			<Exercises />
+		</Box>
+	);
 };
 
 export default Home;
